7abda644f128e5095a8b3fd90e9d5a5c09e591a5
croustibzh <m.ledot@outlook.com>
2019-12-09 00:46:46 -0500

    Added endpoints for GET specific player (by ID) and delete player by ID

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+let ObjectId = require('mongoose').Types.ObjectId;
 
-app.use((req,res,next)=>{
+
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-Width, Content-Type, Accept")
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, PUT, OPTIONS");
@@ -13,7 +15,7 @@ app.use((req,res,next)=>{
 const Player = require('./Model/player');
 
 mongoose.connect('mongodb+srv://comp3123:admin@cluster0-3eshe.mongodb.net/playersDB?retryWrites=true&w=majority')
-    .then(()=>{
+    .then(() => {
         console.log('Connected to database');
     })
     .catch(() => {
@@ -21,34 +23,60 @@ mongoose.connect('mongodb+srv://comp3123:admin@cluster0-3eshe.mongodb.net/player
     });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.post('/api/players', (req,res,next)=>{
+app.post('/api/players', (req, res, next) => {
     const player = new Player({
-        username:req.body.username,
-        rank:req.body.rank,
-        score:req.body.score,
-        time:req.body.time,
-        fGame:req.body.fGame,
-        status:req.body.status
+        username: req.body.username,
+        rank: req.body.rank,
+        score: req.body.score,
+        time: req.body.time,
+        fGame: req.body.fGame,
+        status: req.body.status
     })
-    
+
     player.save((err, player) => {
         if (err) return console.error(err);
         console.log(player.name + " saved to players collection.");
-      });
+    });
 
     res.status(201).json({
         message: "Added successfully to db"
-    }); 
-});
-
-app.get('/api/players',(req,res,next)=>{
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        //posts:posts
     });
 });
 
+app.get('/api/players', (req, res, next) => {
+    Player.find((err, docs) => {
+        if (!err) { res.send(docs); }
+        else { console.log("Error : " + JSON.stringify(err, undefined, 2)) }
+    });
+    // res.status(200).json({
+    //     message: 'Players fetched successfully!',
+    // });
+});
+
+app.get('/api/players/:id', (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send('No record with given id: ${req.params.id}');
+    else {
+
+        Player.findById(req.params.id, (err, doc) => {
+            if (!err) { res.send(doc); }
+            else { console.log("Error fetching player: " + JSON.stringify(err, undefined, 2)) }
+        });
+    }
+});
+
+app.delete('api/players/:id', (req,res,next) =>{
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send('No record with given id: ${req.params.id}');
+    else {
+
+        Player.findByIdAndRemove(req.params.id, (err, doc) => {
+            if (!err) { res.send(doc); }
+            else { console.log("Error deleting player : " + JSON.stringify(err, undefined, 2)) }
+        });
+    }
+})
 module.exports = app;
